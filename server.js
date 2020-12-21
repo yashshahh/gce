da6da8ac867ed18da440ceacc7d9c1ea2d5fafd1
yashshahh <31651852+yashshahh@users.noreply.github.com>
2020-12-21 18:25:47 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-console.log('Server starting...');
-http.createServer((req, res) => {
-    console.log('Request received...');
-    res.end('Hello GCP dev!');
-}).listen(80, () => {
-    console.log('started.');
-});
